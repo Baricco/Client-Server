898baf80ea8ae517942845db274cce4074581250
Filippo Barucci <filibarucci@gmail.com>
2021-03-15 02:09:54 +0100

    Ho diviso correttamente i metodi del client e ho eliminato la variabile msg perchè se ne può fare a meno

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -8,8 +8,7 @@ public class Client
     String serverIp;
     static BufferedReader in;
     static BufferedWriter out;
-    BufferedReader scanner;
-    String msg;
+    static BufferedReader scanner;
 
     //ip Baricco 87.20.39.3
 
@@ -20,18 +19,24 @@ public class Client
         scanner = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    public void communicate() {
-        
-        System.out.println("[2] - Type the message to write to the Server: ");
-        try { msg = scanner.readLine(); } catch (IOException e) { return; }
-        
+    public void sendMessage(String msg) {        
         System.out.println("[3] - Sending: " + msg + "...");
         try { out.write(msg + "\n"); out.flush();} catch (IOException e) { System.out.println("Error, can't output to the server"); return; }
-        
+    }
+
+    private void getServerReply() {
         System.out.println("[4] - Waiting Server's reply...");
+        
+        String msg = "";
+        
         try { msg = in.readLine(); } catch (IOException e) { e.printStackTrace(); System.out.println("Error, can't get input from the server"); return; }
         
         System.out.println("[5] - Server's Reply: " + msg);
+    }
+
+    private static String getUserInput() {
+        System.out.println("[2] - Type the message to write to the Server: ");
+        try { return scanner.readLine(); } catch (IOException e) { return null; }
     }
 
     public Socket connect() {
@@ -53,7 +58,21 @@ public class Client
     public static void main(String args[]) {
         Client client = new Client();
         Socket socket = client.connect(); 
-        if(socket != null) client.communicate();
+        
+        String msg = "";
+
+        do {
+            msg = getUserInput();
+            client.sendMessage(msg);
+            client.getServerReply();
+        } while (!msg.equals("stop"));
+        /*
+        in linea di massima funziona ma il client
+        manda al sever il messaggio "stop" quando vuole terminare la connessione
+        e il server gli risponde e non ha senso e poi non si possono mandare le lettere 
+        accentate (mi sa che è perchè in ASCII non ci sono le lettere accentate maiuscole)
+        */
+        client.sendMessage("STOP_CONNECTION");
     }
 
 
