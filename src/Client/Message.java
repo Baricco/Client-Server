875e2765f8c2ecb712a312ef4d33cca8f980bf5c
Filippo Barucci <filibarucci@gmail.com>
2021-06-01 13:28:20 +0200

    pedrazzi se togli i gruppi temporanei ti stacco il cazzo e lo cucino al forno

--- src/Client/Message.java
+++ src/Client/Message.java
@@ -1,3 +1,26 @@
+/*import java.io.Serializable;
+
+public class Message implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    public String content;
+    public String group;
+    public String username;
+    
+    public Message(String username, String group, String content) {
+        this.content = content;
+        this.group = group;
+        this.username = username;
+    }
+
+    public Message() { 
+        this.content = "";
+        this.group = "";
+        this.username = "";
+    }
+
+    
+}*/
 import java.io.Serializable;
 
 public class Message implements Serializable {
