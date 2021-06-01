41d351dccce953dac44ca035757cf56499a395ab
IlPedraz <pedrazzilorenzo@istitutocorni.it>
2021-06-01 13:49:28 +0200

    h

--- src/Client/Message.java
+++ src/Client/Message.java
@@ -1,26 +1,3 @@
-/*import java.io.Serializable;
-
-public class Message implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-    public String content;
-    public String group;
-    public String username;
-    
-    public Message(String username, String group, String content) {
-        this.content = content;
-        this.group = group;
-        this.username = username;
-    }
-
-    public Message() { 
-        this.content = "";
-        this.group = "";
-        this.username = "";
-    }
-
-    
-}*/
 import java.io.Serializable;
 
 public class Message implements Serializable {
