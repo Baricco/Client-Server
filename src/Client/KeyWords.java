e64a4a28e4b57e6735603917ec5d3dc1610705eb
Filippo Barucci <filibarucci@gmail.com>
2021-06-21 15:30:41 +0200

    ip changed

--- src/Client/KeyWords.java
+++ src/Client/KeyWords.java
@@ -16,7 +16,7 @@ public interface KeyWords {
     public static final String[] groupExpirations = { "1 hour", "3 hours", "6 hours", "12 hours", "24 hours", "7 days", "Permanent" }; 
 
     //                  VALORI DIFFERENTI 
-    public static final String SERVER_IP = "79.53.39.244";
+    public static final String SERVER_IP = "95.235.45.81";
     public static final Group GLOBAL_CHAT = new Group("0000", "Global Chat");
 
     public static final int DURATION_MILLIS = 5000;
