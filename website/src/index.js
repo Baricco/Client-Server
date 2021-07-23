df37d8594207ec634f0144f16da08fa5418f8ccb
Baricco <filibarucci@gmail.com>
2021-07-23 22:58:55 +0200

    prova triangolozzi

--- website/src/index.js
+++ website/src/index.js
@@ -4,11 +4,13 @@ import './styles/index.css';
 import Footer from './Footer';
 import Header from './Header';
 import NavBar from './NavBar';
+import TriangleSection from './TriangleSection';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header/>
     <NavBar/>
+    <TriangleSection/>
     <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
