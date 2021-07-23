1fa399adcc8ecf179ff913093ec54e3bfbf116ea
IlPedraz <pedrazzilorenzo@istitutocorni.it>
2021-07-24 00:57:45 +0200

    trtrt

--- website/src/TriangleSection.jsx
+++ website/src/TriangleSection.jsx
@@ -1,6 +1,5 @@
 import "./styles/TriangleSection.css"
 
-<<<<<<< HEAD
 export function TriangleSection(a, b) {
     if (b < 1) {
         return ( <
@@ -10,10 +9,10 @@ export function TriangleSection(a, b) {
             <
             center >
             <
-            h1 > { a } < /h1>   <
-            /center >  <
-            /div>   <
-            /div>
+            h1 > { a } < /h1>   < /
+            center > <
+            /div>   < /
+            div >
         );
     } else {
         return ( <
@@ -23,24 +22,12 @@ export function TriangleSection(a, b) {
             <
             center >
             <
-            h1 id = "h1Bello" > { a } < /h1>   <
-            /center >  <
-            /div>   <
-            /div>
+            h1 id = "h1Bello" > { a } < /h1>   < /
+            center > <
+            /div>   < /
+            div >
         );
     }
-=======
-export function TriangleSection(a) {
-    return ( 
-        <div id = "InnerDiv">
-            <div id = "TrapezoidShape">
-                <center>
-                    <h1> { a } </h1>  
-                </center> 
-            </div>
-        </div>
-    );
->>>>>>> a5a0304b7bb24d8587859e3c61c37046a379a5ec
 }
 
 export default TriangleSection;
--- website/src/index.jsx
+++ website/src/index.jsx
@@ -6,7 +6,6 @@ import { TriangleSection } from './TriangleSection'
 import Header from './Header';
 import NavBar from './NavBar';
 
-<<<<<<< HEAD
 ReactDOM.render( <
     React.StrictMode >
     <
@@ -18,13 +17,5 @@ ReactDOM.render( <
     Footer / >
     <
     /React.StrictMode>,
-=======
-ReactDOM.render(
-    <React.StrictMode>
-    <Header/>
-    <NavBar/> { TriangleSection("ciao") } 
-    <Footer/>
-    </React.StrictMode>,
->>>>>>> a5a0304b7bb24d8587859e3c61c37046a379a5ec
     document.getElementById('root')
 );
